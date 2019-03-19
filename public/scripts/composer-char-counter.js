31708f31c041a282c9ee3c4692a126f3ac19a6b3
Xia Dong <dongxiapp@gmail.com>
2019-03-19 21:08:08 +0000

    update counter on the page

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-  $(".new-tweet form textarea").on('keyup', function(){
-    if($(this).val().length <= 140){
-      console.log('input length:', $(this).val().length);
+  $(".new-tweet form textarea").on('input', function(){
+    const inputed = $(this).val().length;
+    if(inputed <= 140){
+      $(this).parent().find(".counter").html(inputed);
+      //console.log('input length:', $(this).val().length);
     } else {
-      console.log('input length:', 140 - $(this).val().length);
+      $(this).parent().find(".counter").html(140 - inputed);
+      //console.log('input length:', 140 - $(this).val().length);
     }
-
   });
 });
