28a2bf1bdc38a71ce5ccaa7e9967d343269e6957
Xia Dong <dongxiapp@gmail.com>
2019-03-20 21:10:02 +0000

    form validation, prevent input that is too long or empty

--- public/scripts/new-tweet.js
+++ public/scripts/new-tweet.js
@@ -1,9 +1,15 @@
 $(".new-tweet form").submit(function(e){
   e.preventDefault();
-  const serialized = $(this).serialize();
-  $.post("tweets", serialized, function(res){
-    //console.log("lala")
-  })//.done(function(){
-  //   console.log("lala")
-  // })
+  const length = $(".new-tweet textarea").val().length;
+  if(length < 1){
+    alert('Empty input!')
+  } else if(length > 140){
+    alert('This tweet is too long!')
+  } else {
+    const serialized = $(this).serialize();
+    $(".new-tweet textarea").val("");//clear the text field
+    $.post("/tweets", serialized, function(){
+      console.log("lala")
+    })
+  }
 })
