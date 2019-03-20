54ac444cbc8852c045a94e4764fd862d2e833bba
Xia Dong <dongxiapp@gmail.com>
2019-03-20 23:40:46 +0000

    replace alert error message with jquery error message

--- public/scripts/new-tweet.js
+++ public/scripts/new-tweet.js
@@ -1,17 +1,22 @@
+$("#error").hide();//hide error message on default
 $(".new-tweet form").submit(function(e){
   e.preventDefault();
   const length = $(".new-tweet textarea").val().length;
   if(length < 1){
-    alert('Empty input!')
+    $("#error").empty();
+    $("#error").slideDown();
+    $("#error").append("Error: Empty input!");
+    
   } else if(length > 140){
-    alert('This tweet is too long!')
+    $("#error").empty();
+    $("#error").slideDown();
+    $("#error").append("Error: This tweet is too long!");
   } else {
     const serialized = $(this).serialize();
     $(".new-tweet textarea").val("");//clear the text field
     $("span.counter").text("0");
+    $("#error").hide();
     $.post("/tweets", serialized, function(){
-      // $("#tweets").empty();
-      // loadTweets();
       $.ajax('tweets', { method: 'GET' })
         .then(function(res){
           const newest = res[res.length - 1];
@@ -24,6 +29,6 @@ $(".new-tweet form").submit(function(e){
 })
 
 $("#compose").on("click", function(){
-  $(".new-tweet").slideToggle();
-  $(".new-tweet textarea").focus();
+  $(".new-tweet").slideToggle(); //toggle the form
+  $(".new-tweet textarea").focus(); //focus on the textarea
 })
